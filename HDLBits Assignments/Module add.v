680a4cb90a672a878a9ef7e30a1751d24b4109c6
mohamed-m-atef <mohammahm804@gmail.com>
2026-08-04 15:47:41 +0200

    Implement 32-bit adder using two 16-bit adders

---
+++ HDLBits Assignments/Module add.v
@@ -0,0 +1,11 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire cout_1,cout_2;
+    add16 A1(.a(a[15:0]),.b(b[15:0]),.cin(1'b0) ,.sum(sum[15:0]),.cout(cout_1));
+    add16 A2(.a(a[31:16]),.b(b[31:16]),.cin(cout_1) ,.sum(sum[31:16]),);
+    
+
+endmodule
