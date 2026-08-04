35185d50c42475e0e21c4a25047d0a1b7dfba858
mohamed-m-atef <mohammahm804@gmail.com>
2026-08-04 15:48:04 +0200

    Create Module fadd.v

---
+++ HDLBits Assignments/Module fadd.v
@@ -0,0 +1,13 @@
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+
+// Full adder module here
+
+endmodule
